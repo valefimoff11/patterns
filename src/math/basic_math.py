b9066d06aed892a4fb2e3c3da354d2dcb6badd7a
valefimoff11 <val.efimoff11@gmail.com>
2025-05-04 19:13:29 +0100

    initial commit

--- src/math/basic_math.py
+++ src/math/basic_math.py
@@ -91,5 +91,10 @@ import numpy as np
 print([1, np.e, np.e**2, 10])
 print(np.log([1, np.e, np.e**2, 10]))
 
+
+print(np.log10([1e-15, 100, -3.0]))
+
 #######################################################
 
+#Pi
+print(np.pi)
