9a894ce59c6e43e44fb7d978582fc7aa6cf502d4
valefimoff11 <val.efimoff11@gmail.com>
2025-05-04 17:16:11 +0100

    initial commit

--- src/math/basic_math.py
+++ src/math/basic_math.py
@@ -1,7 +1,37 @@
-#raise x (term) to the power of n (exponent)
+#1. English math terms for all situations
+#2. Coding any math expression/equation in python
+#3. Feeding the above with data / data processing / preparation for the above
+
+import math
+
+#######################################################
+
+#Raise to the Power of N / Exponent
+#raise x (base) to the power of n (exponent)
 
 x = 2
 n = 3
 
 print(x**n)
 #output: 8
+
+#######################################################
+
+#Modulo
+#10 % 4 gives 2 because 10 divided by 4 leaves a remainder of 2.
+
+rem = 10 % 4
+print(rem)
+
+#######################################################
+
+
+#Sqrt
+#Power
+#Modulo
+#Loharithm
+#Etc
+
+#Exponent
+#From math, np and basic python funcs - source all these
+#Find e where to source
