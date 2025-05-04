1a4413c721412f424f4e90c22b4f3e25fff56db1
valefimoff11 <val.efimoff11@gmail.com>
2025-05-04 18:22:44 +0100

    initial commit

--- src/math/math_models/math_models.py
+++ src/math/math_models/math_models.py
@@ -7,8 +7,8 @@ def compound_interest(principal, rate, time_period, compounds_per_year):
 initial_investment = 1000
 interest_rate = 0.08
 years = 5
-compouds_per_year = 12
-result = compound_interest(initial_investment, interest_rate, years, compouds_per_year)
+compounds_per_year = 12
+result = compound_interest(initial_investment, interest_rate, years, compounds_per_year)
 
 print(f"Total value: {result:.2f}")
 
