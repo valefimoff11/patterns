5d9155d4a85ae86b728ccb8b6bf2fca380e81f3d
valefimoff11 <val.efimoff11@gmail.com>
2025-05-04 17:35:26 +0100

    initial commit

--- src/math/basic_math.py
+++ src/math/basic_math.py
@@ -3,11 +3,13 @@
 #3. Feeding the above with data / data processing / preparation for the above
 
 import math
+import numpy as np
 
 #######################################################
 
 #Raise to the Power of N / Exponent
 #raise x (base) to the power of n (exponent)
+#Exponentiation, (power) x to the power y
 
 x = 2
 n = 3
@@ -23,9 +25,20 @@ print(x**n)
 rem = 10 % 4
 print(rem)
 
+#the rules for calculating modulo for negative values are differnt
+print( 15 % -7)
+print(15/-7)
+
+#######################################################
+
+#Floor division - returns the whole part rather than the remainder, in this way complements modulo
+print(10//6)
+
 #######################################################
 
 
+
+#exp
 #Sqrt
 #Power
 #Modulo
