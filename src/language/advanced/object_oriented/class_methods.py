59eb089079ade55d0f2166c4289a128908b6c3d4
valefimoff11 <val.efimoff11@gmail.com>
2025-05-09 19:38:57 +0100

    initial commit

---
+++ src/language/advanced/object_oriented/class_methods.py
@@ -0,0 +1,39 @@
+import sys
+
+class MethodDemo:
+
+    #class (static) attributes
+    y = 100
+
+    def __init__(self):
+
+        #object instance attributes
+        self.x = 1
+
+    #string representation of the object
+    def __repr__(self):
+        return f"Value: {self.x}"
+
+    @classmethod
+    def class_method_demo(cls):
+        cls.y = 99
+        print(cls.y)
+
+    def object_instance_method_demo(self):
+        print(self.x)
+        print(self.y)
+
+#direct access to class (static) variable through the class (object) itself
+print(MethodDemo.y)
+
+#access to class method through the class (object) itself
+MethodDemo.class_method_demo()
+
+o = MethodDemo()
+
+#access to class method through object handle
+o.class_method_demo()
+
+o.object_instance_method_demo()
+
+print(o)
