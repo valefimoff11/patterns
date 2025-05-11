60a0b5d1390339b3f80d1c399cf9e0a45ae9393a
valefimoff11 <val.efimoff11@gmail.com>
2025-05-11 21:52:46 +0100

    initial commit

---
+++ src/frameworks/pandas/basic.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+data = {
+     'name': ['Xavier', 'Ann', 'Jana', 'Yi', 'Robin', 'Amal', 'Nori'],
+     'city': ['Mexico City', 'Toronto', 'Prague', 'Shanghai',
+              'Manchester', 'Cairo', 'Osaka'],
+     'age': [41, 28, 33, 34, 38, 31, 37],
+     'py-score': [88.0, 79.0, 81.0, 80.0, 68.0, 61.0, 84.0]
+}
+
+row_labels = [101, 102, 103, 104, 105, 106, 107]
+
+
+df = pd.DataFrame(data=data, index=row_labels)
+
+print(df)
+
+cities = df['city']
+print(cities)
+print(df.city)
+
+print(df.iloc[:, 1])
