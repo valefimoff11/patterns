310fa57d442f64eecdcb9c2ae5cf824d709f205d
valefimoff11 <val.efimoff11@gmail.com>
2025-05-12 22:54:03 +0100

    initial commit

--- src/language/datetime/pandas_time_m1.py
+++ src/language/datetime/pandas_time_m1.py
@@ -43,3 +43,50 @@ for day in week:
 
 df = pd.read_csv('E:\\pyprojects\\patterns\\patterns\\sampledata\\server_util.csv')
 print(df.head())
+
+print(df.info())
+
+df['datetime'] = pd.to_datetime(df['datetime'])
+
+print(df.info())
+
+df = pd.read_csv('E:\\pyprojects\\patterns\\patterns\\sampledata\\server_util.csv', parse_dates=['datetime'])
+print(df.head())
+
+print(df.datetime.min())
+print(df.datetime.max())
+
+mask = (df.datetime >= pd.Timestamp('2019-03-06')) & (df.datetime < pd.Timestamp('2019-03-07'))
+print(df.loc[mask])
+
+df.set_index('datetime', inplace=True)
+print(df)
+
+print(df.loc['2019-03-07 02:00:00'].head(5))
+print(df.loc['2019-03-07'].head(5))
+
+df.loc['Apr 2019']
+df.loc['8th April 2019']
+df.loc['April 05, 2019 5pm']
+
+#print(df.loc['03-04-2019':'04-04-2019'])
+print(df.sort_index().loc['03-04-2019':'04-04-2019'])
+
+print(type(df.index))
+print(df.at_time('09:00'))
+
+print(df.between_time('00:00','02:00'))
+
+print(df.sort_index().first('5B'))
+
+print(df.sort_index().last('1W'))
+
+df.sort_index().last('2W')
+
+df[df.server_id == 100].resample('D')['cpu_utilization', 'free_memory', 'session_count'].mean()
+
+df.groupby(df.server_id).resample('M')['cpu_utilization', 'free_memory'].max()
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots(figsize=(24, 8))
+df.groupby(df.server_id).resample('M')['cpu_utilization'].mean().plot.bar(color=['green', 'gray'], ax=ax, title='The Average Monthly CPU Utilization Comparison')
