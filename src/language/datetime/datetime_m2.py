742815731389b346435df3aa1a12fa1489111b59
valefimoff11 <val.efimoff11@gmail.com>
2025-05-13 01:31:50 +0100

    initial commit

--- src/language/datetime/datetime_m2.py
+++ src/language/datetime/datetime_m2.py
@@ -1,9 +1,15 @@
 from dateutil import parser
 #from datetime import datetime
 
-# Date/Time Validation Rule Types
+# Date/Time Validation Rule Types (all validation rule types are also implemented in pandas and numpy date/time handling functions)
 # 1. Date/Time Validation and Parsing through Explicitly/upfront specified, Date/time specific regular expressions
-# 2. Fully Automated date/time valdation and parsing - no upfront reg expression spec
+# 2. Fully Automated date/time validation and parsing - no upfront reg expression spec
+
+###############################################################
+# 1. Date/Time Validation and Parsing through Explicitly/upfront specified, Date/time specific regular expressions
+###############################################################
+
+
 
 ###############################################################
 # 2. Fully Automated date/time validation and parsing - no upfront reg expression spec required - however contains risks of semantically incorrect
@@ -15,10 +21,18 @@ date_str1 = "2024-09-16"
 date_str2 = "16th September, 2024"
 date_str3 = "09/16/2024"
 
-# Parsing date strings
-date1 = parser.parse(date_str1)
-date2 = parser.parse(date_str2)
-date3 = parser.parse(date_str3)
+try:
+
+    # Parsing date strings
+    date1 = parser.parse(date_str1)
+    date2 = parser.parse(date_str2)
+    date3 = parser.parse(date_str3)
+
+# If the date validation goes wrong
+except ValueError:
+
+   # printing the appropriate text if ValueError occurs
+   print("Incorrect data format")
 
 print("Parsed Date 1:", date1)
 print("Parsed Date 2:", date2)
